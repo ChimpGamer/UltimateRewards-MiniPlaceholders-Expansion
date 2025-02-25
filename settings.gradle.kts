045835b3e8497d4e757903d91277b73fa871da0e
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-02-25 18:02:01 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "UltimateRewards-MiniPlaceholders-Expansion"
---
+++ src/main/kotlin/nl/chimpgamer/ultimaterewards/miniplaceholders/expansion/UltimateRewardsMiniPlaceholdersExpansion.kt
@@ -0,0 +1,14 @@
+package nl.chimpgamer.ultimateRewardsMiniPlaceholdersExpansion
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class UltimateRewardsMiniPlaceholdersExpansion : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
